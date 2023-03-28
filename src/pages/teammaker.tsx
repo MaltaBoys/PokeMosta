750ec3829cabb3776f950bbfe22adb57f782757b
Adr1 <adriangelmotto@gmail.com>
2023-03-28 10:27:35 +0200

    Added TeamMaker Page

---
+++ src/pages/teammaker.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function TeamMaker() {
+  return (
+    <>
+      <Head>
+        <title>TeamMaker</title>
+        <meta name="description" content="Team Maker" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
+
