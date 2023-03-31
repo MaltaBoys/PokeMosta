4b1ae73ff380fed109266c36d69be233f33da385
Natalio Rabasco <nataliorabasconavarro@gmail.com>
2023-03-31 10:45:31 +0200

    Borrada la configuración de next

--- src/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import 'maltaboys/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
