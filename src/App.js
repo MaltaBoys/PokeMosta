9eaeaf37de4acd9eb3e616d8c18213877c2a76e8
MateoMartinezCh <mateomartinezchazarra@gmail.com>
2023-04-02 10:24:30 +0200

    Redo of toogle dark mode, now keeps the user preference between the whole app

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import { Menus, Home, Tcg, Pokedex, Pokemon } from "./hooks";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <Flowbite>
-      <header>
-        <Menus />
-      </header>
-      <main>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/pokedex" element={<Pokedex />} />
-            <Route path="/tcg" element={<Tcg />} />
-            <Route path="/pokemon/:id" element={<Pokemon />} />
-          </Routes>
-        </Router>
-      </main>
-    </Flowbite>
-  );
+	return (
+		<Flowbite>
+			<header>
+				<Menus />
+			</header>
+			<main>
+				<Router>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/pokedex" element={<Pokedex />} />
+						<Route path="/tcg" element={<Tcg />} />
+						<Route path="/pokemon/:id" element={<Pokemon />} />
+					</Routes>
+				</Router>
+			</main>
+		</Flowbite>
+	);
 }
 
 export default App;
