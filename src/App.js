3ca0ba24c9a7d1021bcdfada146517f8c9ce9f25
Natalio Rabasco <nataliorabasconavarro@gmail.com>
2023-04-03 16:50:51 +0200

    WIP:Created PokemonDetail section, add new font and add new comments

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import { Flowbite } from "flowbite-react";
-import { Menus, Home, Tcg, Pokedex } from "./hooks";
+import { Menus, Home, Tcg, Pokedex, ScrollTop } from "./hooks";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CardDetail from "./components/Cards/PokemonCard/CardDetail";
 
 function App() {
-	return (
-		<Flowbite>
-			<header>
-				<Menus />
-			</header>
-			<main>
-				<Router>
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/pokedex" element={<Pokedex />} />
-						<Route path="/tcg" element={<Tcg />} />
-						<Route path="/pokemon/:id" element={<CardDetail />} />
-					</Routes>
-				</Router>
-			</main>
-		</Flowbite>
-	);
+  return (
+    <Flowbite>
+      <header>
+        <Menus />
+      </header>
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/pokedex" element={<Pokedex />} />
+            <Route path="/tcg" element={<Tcg />} />
+            <Route path="/pokemon/:id" element={<CardDetail />} />
+          </Routes>
+        </Router>
+      </main>
+      <ScrollTop />
+    </Flowbite>
+  );
 }
 
 export default App;
--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -4,6 +4,9 @@ export { default as PokemonCard } from "../components/Cards/PokemonCard/Card";
 export { default as PokemonList } from "../components/Cards/PokemonCard/ListCards";
 export { default as Pokemon } from "../components/Cards/PokemonCard/CardDetail";
 
+// Functions
+export { default as ScrollTop } from "../components/Functions/ScrollTop";
+
 // Pages
 export { default as Home } from "../pages/Home";
 export { default as Pokedex } from "../pages/Pokedex";
