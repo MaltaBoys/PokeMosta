629b6f7783942997e22d778fb282a9d51b96581e
Adr1 <adriangelmotto@gmail.com>
2023-04-10 12:24:33 +0200

    Added loggin and fixed Bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import { Flowbite } from "flowbite-react";
-import { Menus, Home, Tcg, Pokedex, ScrollTop, Register, Login, User } from "./hooks";
+import {
+  Menus,
+  Home,
+  Tcg,
+  Pokedex,
+  ScrollTop,
+  Register,
+  Login,
+  User,
+} from "./hooks";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CardDetail from "./components/Cards/PokemonCard/CardDetail";
 
@@ -16,12 +25,12 @@ function App() {
             <Route path="/pokedex" element={<Pokedex />} />
             <Route path="/tcg" element={<Tcg />} />
             <Route path="/pokemon/:id" element={<CardDetail />} />
+            <Route path="/user" element={<User />} />
           </Routes>
         </main>
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/user" element={<User />} />
         </Routes>
         <ScrollTop />
       </Router>
